1aba084ad96e27c7c641fac8b490e381e4442e6d
Luo Boming <la_float_765millionstars@yahoo.co.jp>
2019-10-15 14:09:29 +0900

    able to save more than 2 images

--- save_instagram_images.rb
+++ save_instagram_images.rb
@@ -15,10 +15,24 @@ driver = Selenium::WebDriver.for(:chrome, options: options)
 
 driver.get(url)
 
-driver.find_elements(:xpath, '//div[@style]/img').each do |elm|
+# Repeatedly click next buttons
+(0..).each do |i|
+   but_elms = driver.find_elements(:xpath, '//button[@class and @tabindex]')
+   if but_elms.empty? || (but_elms.size == 1 && i > 0)
+      break
+   else
+      next_but = but_elms.last
+      next_but.click
+   end
+end
+
+# Save all attached images
+img_elms = driver.find_elements(:xpath, '//div[@style]/img').each do |elm|
    img_url = elm.attribute('src')
    img = open(img_url).read
    fname = File.basename(URI.parse(img_url).path)
    File.open(fname, 'wb').write(img)
 end
+
+puts "==> Saved #{img_elms.size} images."
 
